8007256cc994c1363f87cbed381b59251f3e0231
goofrost <quentingaronne1@gmail.com>
2024-11-28 13:21:04 +0100

    update new one

--- container-app/src/app/app.component.spec.ts
+++ container-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular_appli' title`, () => {
+  it(`should have the 'container-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular_appli');
+    expect(app.title).toEqual('container-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular_appli');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, container-app');
   });
 });
